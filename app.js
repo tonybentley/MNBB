4599df6e367d5de48ada74958cad9e2b663a2797
Tony Bentley <tony@tonybentley.com>
2014-06-12 00:02:16 -0700

    nmea parser shows json for supported sentences and unparsed sentences for unsupported sentences. Testing on pi

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
 var net = require('net');
 
-var nmea   = require('nmea-0183');
+/*
+npm module alias = nmea-0183
+forked repo = node-nmea under ./node-modules directory
+*/
+
+var nmeaParser = require('node-nmea');
+
 
 var HOST = '192.168.0.101';
 var PORT = 39150;
@@ -11,7 +17,7 @@ var net = require('net');
 var client = new net.Socket();
 client.setEncoding('utf8');
 client.connect(PORT, HOST, function() {
-    console.log("connected to NMEA TCP Client")
+    console.log("Connected to NMEA TCP Client")
 });
  
 client.on('data', function(data) {
@@ -30,35 +36,16 @@ function parseDataPackets(packet){
         var line = lines[i];
         //get the unique key
         var key = line.split(",")[0].replace(/\W+/g, "");;
+        //if a parser is available.... otherwise an error gets thrown
         try{
-            var jsonNmea = nmea.parse(line);
-            console.log(jsonNmea)
+            //if 
+            var jsonNmea = nmeaParser.parse(line);
+            console.log("parsed: "+key);
+            console.log(jsonNmea);
         }
         catch(e){
-            console.log(e)
+           console.log(line)
         }
         
-
-
-console.log(jsonNmea);
-        /*if(keys.indexOf(key) === -1){
-            keys.push(key);
-            console.log(line)
-        }
-        else{
-            console.log("dup");
-        }*/
     }
-
-
-}
-
-function uniqueKeys(objects) {
-  var keys = objects.reduce(function (collector, obj) {
-    Object.keys(obj).forEach(function (key) {
-      collector[key] = true;
-    });
-    return collector;
-  }, {});
-  return Object.keys(keys);
 }
